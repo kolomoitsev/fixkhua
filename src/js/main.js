7efe35df1625a24b6b9ebc9879ced9bf7345751b
kolomoitsev <Pavel27011999138>
2020-09-15 13:10:54 +0300

    slider

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // slick carousel
+    $('.sliderWrapper').slick({
+        vertical: true,
+        dots: true,
+        arrows: false,
+        infinite: false,
+        autoplay: false,
+        slidesToShow: 1,
+        responsive: true
+    });
+});
